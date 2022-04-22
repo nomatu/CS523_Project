dc34cb71ab5e200a45e8cb0ebdb1356884461ee8
Iden Kalemaj <58532526+iden-kalemaj@users.noreply.github.com>
2022-04-22 14:20:59 -0400

    Update vision.py

--- vision.py
+++ vision.py
@@ -4,15 +4,18 @@ parser = argparse.ArgumentParser(description='SIDPSGD')
 parser.add_argument('--dataset', default='mnist')
 parser.add_argument('--noise_multiplier', type=float, default=7.1)
 parser.add_argument('--clip', type=float, default=3.1)
+parser.add_argument('--private', type=int, default=1)
 parser.add_argument('--lr', type=float, default=0.1)
 parser.add_argument('--batch_size', type=int, default=32)
 parser.add_argument('--epochs', type=int, default=10)
 parser.add_argument('--normalization_type', default='Batch')
 parser.add_argument('--device', default=None)
-parser.add_argument('--robust', default=False)
-parser.add_argument('--adv_step_size', default=0.01)
-parser.add_argument('--adv_alpha', default=0.3)
-parser.add_argument('--adv_num_steps', default=40)
+parser.add_argument('--robust', type=int, default=0)
+parser.add_argument('--adv_step_size', type=float, default=0.01)
+parser.add_argument('--adv_alpha', type=float, default=0.3)
+parser.add_argument('--adv_num_steps', type =int, default=20)
+parser.add_argument('--resume_at_epoch', type =int, default=0)
+
 
 args = parser.parse_args()
 
@@ -22,6 +25,6 @@ elif args.dataset == 'cifar10':
      from src.cifar10.train import main
 
 
-main(args.noise_multiplier, args.clip, args.lr, args.batch_size, args.epochs, args.normalization_type, args.device,
-     args.robust, args.adv_step_size, args.adv_alpha, int(args.adv_num_steps))
+main(args.noise_multiplier, args.clip, args.private, args.lr, args.batch_size, args.epochs, args.normalization_type, args.device,
+     args.robust, args.adv_step_size, args.adv_alpha, args.adv_num_steps, args.resume_at_epoch)
 
