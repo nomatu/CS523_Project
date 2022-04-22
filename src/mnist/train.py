ca96b1d0a9d6c526b3c9adc7c854e92a93aa414d
Iden Kalemaj <58532526+iden-kalemaj@users.noreply.github.com>
2022-04-22 14:22:43 -0400

    Update train.py

--- src/mnist/train.py
+++ src/mnist/train.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 from torchvision import datasets
 from torch.utils.data import DataLoader, RandomSampler
 import torchvision.transforms as transforms
@@ -6,16 +7,24 @@ from tqdm import tqdm
 from .dataset import data_loaders, axi_loader
 from src.pgd_attack import projected_gradient_descent, viz_adversarial
 
-def train(model, criterion, optimizer, device, train_loader, clip, noise_multiplier, batch_size, axi_x, robust, adv_step_size, adv_alpha, adv_num_steps):
+import tensorflow as tf
+from tensorflow_privacy.privacy.analysis import compute_dp_sgd_privacy_lib
+
+
+def train(model, criterion, optimizer, device, train_loader, clip, noise_multiplier, private, batch_size, axi_x, robust, adv_step_size, adv_alpha, adv_num_steps):
     model.train()
+    loss_fn_robust = torch.nn.CrossEntropyLoss()
     for x, y in tqdm(train_loader):
         _lr = optimizer.param_groups[0]['lr']
-        std_params = _lr * clip * noise_multiplier / batch_size
-        
+        if private:
+          std_params = _lr * clip * noise_multiplier / np.sqrt(batch_size)
+        else:
+          std_params = 0.0
+
         x_adv = x
         
         if robust:
-          x_adv = projected_gradient_descent( model, x, y, loss_fn = criterion, std_params = std_params, 
+          x_adv = projected_gradient_descent( model, x, y, loss_fn = loss_fn_robust, std_params = std_params, 
                                               axi_x = axi_x, device = device, step_size = adv_step_size, 
                                               num_steps = adv_num_steps, alpha = adv_alpha) 
         
@@ -34,7 +43,8 @@ def train(model, criterion, optimizer, device, train_loader, clip, noise_multipl
 
         for j in losses:
             j.backward(retain_graph=True)
-            torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+            if private:
+              torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
             for tensor_name, tensor in model.named_parameters():
                 new_grad = tensor.grad
                 saved_var[tensor_name].add_(new_grad)
@@ -61,13 +71,14 @@ def evaluate_accuracy(model, data_loader, device, axi_x):
     accuracy = correct / total
     return accuracy
 
-def main(noise_multiplier, clip, lr, batch_size, epochs, normalization_type, device, 
-          robust, adv_step_size, adv_alpha, adv_num_steps):
+def main(noise_multiplier, clip, private, lr, batch_size, epochs, normalization_type, device, 
+          robust, adv_step_size, adv_alpha, adv_num_steps, resume_at_epoch):
     from .model import LeNet5
 
     if device is None:
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+    print(device)
     model = LeNet5(normalization_type)
     model = model.to(device)
     criterion = torch.nn.CrossEntropyLoss(reduction='none')
@@ -79,17 +90,16 @@ def main(noise_multiplier, clip, lr, batch_size, epochs, normalization_type, dev
 
     for epoch in range(epochs):
         
-        train(model, criterion, optimizer, device, train_loader, clip, noise_multiplier, 
+        train(model, criterion, optimizer, device, train_loader, clip, noise_multiplier, private, 
               batch_size, axi_x, robust, adv_step_size, adv_alpha, adv_num_steps)
         scheduler_lr.step()
         test_accuracy = evaluate_accuracy(model, test_loader, device, axi_x)
 
         print('epoch', epoch)
         print('valid accuracy  ', test_accuracy)
+        eps, _ = compute_dp_sgd_privacy_lib.compute_dp_sgd_privacy(60000, batch_size, noise_multiplier, epoch+1, 1e-5)
+        #print('For delta=1e-5, the current epsilon is: %.2f' % eps)
         print('---------------------')
-
-
-
 
 
 
